444976e2a311a119fe9104ee494d469adec6886a
Derek  Burgman <dereekb@gmail.com>
2013-10-10 23:03:48 -0500

    Created BoardReader and MoveFinder
    - Added ReversiBoardReader for easily retrieving relative positions
    - Added ReversiMoveFinder to figure out possible moves. Algorithm
    untested, but was converted from the move-finding algorithm implemented
    in another game.

---
+++ Reversi/src/reversi/game/board/ReversiMoveFinder.java
@@ -0,0 +1,103 @@
+package reversi.game.board;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import reversi.models.ReversiBoard;
+import reversi.models.ReversiEntity;
+import reversi.models.ReversiPlayer;
+import base.models.Position;
+
+/**
+ * Finds moves on the board.
+ * 
+ * 
+ * @author dereekb
+ * 
+ *         Available Positions algorithm derived from algorithm in reversi game
+ *         at: http://mnemstudio.org/game-reversi-intro.htm
+ */
+public class ReversiMoveFinder {
+
+	public static final Integer rowsCount = 8;
+	public static final Integer columnsCount = 8;
+
+	private final ReversiBoard board;
+	private final ReversiPlayer player;
+
+	public ReversiMoveFinder(ReversiBoard board, ReversiPlayer player) {
+		this.board = board;
+		this.player = player;
+	}
+
+	public Set<Position> findMoves() {
+		Set<Position> possibleMoves = new HashSet<Position>();
+
+		for (int c = 0; c < ReversiBoardReader.rowsCount; c++) {
+			String currentColumn = ReversiBoardReader.columnAlpha[c];
+
+			for (int r = 0; r < ReversiBoardReader.columnsCount; r++) {
+				Integer currentRow = r;
+
+				Position currentPosition = new Position(currentColumn,
+						currentRow);
+
+				ReversiEntity entity = this.board
+						.getEntityAtPosition(currentPosition);
+
+				if (entity != null) {
+					ReversiPlayer owner = entity.getOwner();
+
+					if (owner == this.player) {
+
+						List<Position> availableMoves = this
+								.findMovesAtPosition(currentPosition);
+						possibleMoves.addAll(availableMoves);
+					}
+				}
+			}
+		}
+
+		return possibleMoves;
+	}
+
+	public List<Position> findMovesAtPosition(Position position) {
+		List<Position> possibleMoves = new ArrayList<Position>();
+		boolean foundMove = false;
+		
+		Integer opponentPieces = 0;
+		List<Position> opponentPositions = new ArrayList<Position>();
+		List<Position> edgePositions = ReversiBoardReader
+				.positionsSurroundingPosition(position);
+
+		for (Position edgePosition : edgePositions) {
+			
+			ReversiEntity entity = this.board.getEntityAtPosition(edgePosition);
+
+			if (entity == null) // Empty
+			{
+				foundMove = true;
+			} else {
+				ReversiPlayer owner = entity.getOwner();
+
+				if (owner != this.player) // Opponent
+				{
+					opponentPieces += 1;
+					opponentPositions.add(edgePosition);
+				}
+			}
+
+			if (foundMove == true && opponentPieces > 0) {
+				boolean emptySpace = (entity == null);
+				
+				if (emptySpace) {	//An empty space makes this move valid
+					possibleMoves.add(edgePosition);
+				}
+			}
+		}
+		
+		return possibleMoves;
+	}
+}
